eb83ca752b344eee68b25140e27205fe2b133940
Shubham-Rasal <bluequbits@gmail.com>
2023-05-07 18:52:29 +0530

    changed url

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const adBanner = document.body.querySelector('.ad-banner');
 console.log(adBanner);
 
 const video = document.createElement('iframe');
-video.src = 'https://www.youtube.com/embed/5qap5aO4i9A';
+video.src = 'https://www.youtube.com/embed/25zjPEMxwbk';
 video.width = '560';
 video.height = '315';
 video.frameborder = '0';
