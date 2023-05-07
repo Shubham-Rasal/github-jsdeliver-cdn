cf3eed9268c501b2db4cfeed6c558ab5fcebc546
Shubham-Rasal <bluequbits@gmail.com>
2023-05-07 18:46:41 +0530

    added script

---
+++ index.js
@@ -0,0 +1,16 @@
+{/* <iframe src="https://www.youtube.com/embed/5qap5aO4i9A" width="560" height="315" frameborder="0" allowfullscreen></iframe> */}
+// 
+
+//embeded video
+
+const adBanner = document.body.querySelector('.ad-banner');
+console.log(adBanner);
+
+const video = document.createElement('iframe');
+video.src = 'https://www.youtube.com/embed/5qap5aO4i9A';
+video.width = '560';
+video.height = '315';
+video.frameborder = '0';
+video.allowfullscreen = 'true';
+video.oncanplay = 'true';
+adBanner.appendChild(video);
